14ee0a537ad6f83478c0453f2cad1f2b359513a1
suraj <suraj14wadje@gmail.com>
2022-07-02 17:49:24 +0530

    add : solution to exercise 4

---
+++ Week-3/ex_4.js
@@ -0,0 +1,23 @@
+function createStack() {
+   let items = [];
+
+  return {
+    push(item) {
+      items.push(item);
+    },
+    pop() {
+      return items.pop();
+    },
+  };
+}
+const stack = createStack();
+stack.push(10);
+stack.push(5);
+stack.items = [1,2,4]
+console.log(stack.pop()); 
+console.log(stack.pop()); 
+
+// output is :
+// 5
+// 10
+

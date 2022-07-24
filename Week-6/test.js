37a87088111f35a48a8b1a5cb6f8127da3d23b50
suraj <suraj14wadje@gmail.com>
2022-07-24 15:42:27 +0530

    add : solution to ex_3

--- Week-6/test.js
+++ Week-6/test.js
@@ -16,3 +16,14 @@ test("largest sum :",()=>{
     }
 })
 
+test("sort 1's and zeros",()=>{
+    const {sort,testCases} = require('./ex_3');
+
+    for(let i = 0;i<testCases.input.length;i++){
+        const input = testCases.input[i];
+        const output = testCases.output[i];
+    
+        const result = sort(input);
+        expect(result.toString()).toBe(output.toString());
+    }
+})

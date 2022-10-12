97cae164f13d417e9862f8e330444dd9b068a956
suraj <suraj14wadje@gmail.com>
2022-10-12 09:45:27 +0530

    add : authentication api's

--- Week-14/controller/auth.js
+++ Week-14/controller/auth.js
@@ -1,5 +1,36 @@
-const auth = (req, res) => {
-  //todo
-};
+const prisma = require("../prisma/prisma");
+const bcrypt = require("bcrypt");
+
+const authController = require("express").Router();
+
+authController.post("/register", async (req, res) => {
+  const { email, password, name } = req.body;
+  //todo validation
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const result = await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+    select: {
+      id: true,
+      email: true,
+      name: true,
+    },
+  });
+  res.send(result);
+});
+
+authController.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findFirst({ where: { email } });
+  if (!user) return res.send("Invalid email or password").status(401);
+
+  const isValid = await bcrypt.compare(user.password, password);
+  if (!isValid) return res.send("Invalid email or password").status(401);
+});
 
 module.exports = auth;

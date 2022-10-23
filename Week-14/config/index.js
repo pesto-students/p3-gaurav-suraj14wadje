1d5dbb0dd04b6b0a896f2a76a8ce3d3a21653b2f
suraj <suraj14wadje@gmail.com>
2022-10-23 20:46:53 +0530

    add : sentry integration for node app

--- Week-14/config/index.js
+++ Week-14/config/index.js
@@ -2,4 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   DB_URI: process.env.DB_URI,
   jwtSecrete: "12345678",
+  SENTRY: process.env.SENTRY,
 };
--- Week-14/server.js
+++ Week-14/server.js
@@ -1,13 +1,42 @@
 require("dotenv").config();
 const express = require("express");
 require("express-async-errors");
-const { PORT } = require("./config");
+const { PORT, SENTRY } = require("./config");
 const router = require("./router");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 
 const server = express();
 
+Sentry.init({
+  dsn: "https://c4f0e70c97f046c6b6353aa522b1ce8c@o4504033454129152.ingest.sentry.io/4504033456291841",
+  // or pull from params
+  // dsn: params.SENTRY_DSN,
+  environment: "dev",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ server }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  // or pull from params
+  // tracesSampleRate: parseFloat(params.SENTRY_TRACES_SAMPLE_RATE),
+});
+
+server.use(Sentry.Handlers.requestHandler());
+server.use(Sentry.Handlers.tracingHandler());
 server.use(express.json());
+server.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 server.use("/api", router);
+
+server.use(Sentry.Handlers.errorHandler());
 
 server.use((_err, _req, res, _next) => {
   console.error(_err);

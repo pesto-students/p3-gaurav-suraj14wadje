7129e2621bbb3cd29b49898687f46df90be7d942
suraj <suraj14wadje@gmail.com>
2022-10-23 20:32:03 +0530

    add : sentry integration

--- Week-10/url-shortner/src/index.js
+++ Week-10/url-shortner/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+Sentry.init({
+  dsn:
+    "https://00d8d64f5a4f47b1b3134efa534f8441@o4504033454129152.ingest.sentry.io/4504033456291840",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
